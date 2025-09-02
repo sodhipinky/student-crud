d7deec649c47a276a5ae75812cc4e394e537bb22
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-02 18:11:40 -0400

    create StudentController for handling student API requests

---
+++ src/main/java/pro/elevateme/student_crud/controller/StudentController.java
@@ -0,0 +1,9 @@
+package pro.elevateme.student_crud.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+}
