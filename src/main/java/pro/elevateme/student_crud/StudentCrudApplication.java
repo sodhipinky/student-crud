f6f52d45137870db76c8958b4945f2b164c1a138
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-02 17:46:46 -0400

    create student model as entity

---
+++ src/main/java/pro/elevateme/student_crud/StudentCrudApplication.java
@@ -0,0 +1,13 @@
+package pro.elevateme.student_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudApplication.class, args);
+	}
+
+}
