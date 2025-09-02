bb250482e4f23c4f0f256b2fde80c9dc35b58a36
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-02 18:13:18 -0400

    add constructor injection for StudentService in StudentController

--- src/main/java/pro/elevateme/student_crud/controller/StudentController.java
+++ src/main/java/pro/elevateme/student_crud/controller/StudentController.java
@@ -2,8 +2,15 @@ package pro.elevateme.student_crud.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.elevateme.student_crud.service.StudentService;
 
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
+
+    public final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 }
