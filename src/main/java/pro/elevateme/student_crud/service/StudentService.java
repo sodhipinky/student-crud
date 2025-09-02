179fd1dc6ead7169e8962e2b3cc4e1f2985c048b
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-02 17:57:38 -0400

    add constructor injection for StudentRepository in StudentService

--- src/main/java/pro/elevateme/student_crud/service/StudentService.java
+++ src/main/java/pro/elevateme/student_crud/service/StudentService.java
@@ -1,7 +1,16 @@
 package pro.elevateme.student_crud.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.elevateme.student_crud.repository.StudentRepository;
 
 @Service
 public class StudentService {
+
+    public final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 }
