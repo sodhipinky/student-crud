cf8e38bc7848f921b0718827e3e68f0466604651
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-09 17:53:46 -0400

    add validation to student model

--- src/main/java/pro/elevateme/student_crud/model/Student.java
+++ src/main/java/pro/elevateme/student_crud/model/Student.java
@@ -1,6 +1,9 @@
 package pro.elevateme.student_crud.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "students")
@@ -8,8 +11,16 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is mandatory")
     private String email;
+
+    @NotBlank(message = "Course is mandatory")
     private String course;
 
     public Student() {
