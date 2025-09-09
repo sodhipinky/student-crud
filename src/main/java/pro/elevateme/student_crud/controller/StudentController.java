76b823700afef493cce8b9acac7ba237a8a84fa1
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-09 15:20:05 -0400

    add the remaining API end points to the controller

--- src/main/java/pro/elevateme/student_crud/controller/StudentController.java
+++ src/main/java/pro/elevateme/student_crud/controller/StudentController.java
@@ -1,12 +1,11 @@
 package pro.elevateme.student_crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.elevateme.student_crud.model.Student;
 import pro.elevateme.student_crud.service.StudentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/students")
@@ -23,5 +22,29 @@ public class StudentController {
     @PostMapping("/create")
     public Student createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
+    }
+
+    //READ ALL
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    //READ BY ID
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable long id) {
+        return studentService.getStudentById(id);
+    }
+
+    //UPDATE
+    @PutMapping("update/{id}")
+    public Student updateStudent(@PathVariable long id, @RequestBody Student updatedStudent) {
+        return studentService.updateStudent(id, updatedStudent);
+    }
+
+    //DELETE
+    @DeleteMapping("delete/{id}")
+    public void deleteStudent(@PathVariable long id) {
+        studentService.deleteStudent(id);
     }
 }
