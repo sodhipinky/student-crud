123fc9b3870aefa8311baba6784f975be7e62265
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-16 18:32:43 -0400

    enable Mockito for testing the service layer

--- src/test/java/pro/elevateme/student_crud/service/StudentServiceTest.java
+++ src/test/java/pro/elevateme/student_crud/service/StudentServiceTest.java
@@ -1,7 +1,10 @@
 package pro.elevateme.student_crud.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class) // Use Mockito's extension for JUnit 5
 class StudentServiceTest {
 
     @Test
