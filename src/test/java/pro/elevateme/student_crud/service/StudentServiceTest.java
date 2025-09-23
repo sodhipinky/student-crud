ff4dd58b201181276a3c86101197882384e6678d
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-09-23 16:36:03 -0400

    mock the student repository and inject the mock into student service

--- src/test/java/pro/elevateme/student_crud/service/StudentServiceTest.java
+++ src/test/java/pro/elevateme/student_crud/service/StudentServiceTest.java
@@ -2,10 +2,19 @@ package pro.elevateme.student_crud.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pro.elevateme.student_crud.repository.StudentRepository;
 
 @ExtendWith(MockitoExtension.class) // Use Mockito's extension for JUnit 5
 class StudentServiceTest {
+
+    @Mock
+    private StudentRepository studentRepository;    // Mock the repository
+
+    @InjectMocks
+    private StudentService studentService; // Inject the mock into the service
 
     @Test
     void createStudent() {
