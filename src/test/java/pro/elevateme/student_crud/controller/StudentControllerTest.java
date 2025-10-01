dcb116bd315b6be3cd1bdcefa8999a7d05348da7
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-10-01 16:35:09 -0400

    inject MockMvc into StudentControllerTest for simulating HTTP requests

--- src/test/java/pro/elevateme/student_crud/controller/StudentControllerTest.java
+++ src/test/java/pro/elevateme/student_crud/controller/StudentControllerTest.java
@@ -1,12 +1,17 @@
 package pro.elevateme.student_crud.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class StudentControllerTest {
+
+    @Autowired  // Inject MockMvc to simulate HTTP requests
+    private MockMvc mockMvc;
 
     @Test
     void getAllStudents() {
