7bd554b1efe7f6a9132e3689e1b2431ded4c75d1
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-10-01 16:54:47 -0400

    add ActiveProfiles annotation and implement getAllStudents test in StudentControllerTest

--- src/test/java/pro/elevateme/student_crud/controller/StudentControllerTest.java
+++ src/test/java/pro/elevateme/student_crud/controller/StudentControllerTest.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class StudentControllerTest {
@@ -14,6 +20,9 @@ class StudentControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void getAllStudents() {
+    void getAllStudents() throws Exception {
+        mockMvc.perform(get("/api/students"))           // Perform a GET request to the /api/students endpoint
+                .andExpect(status().isOk())                         // Expect HTTP 200 OK status
+                .andExpect(jsonPath("$").isArray());    // Expect the response to be a JSON array
     }
 }
