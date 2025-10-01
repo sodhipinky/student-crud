d9a743402e8d6fc6ea081ccb95040dc42ef236f2
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-10-01 16:33:17 -0400

    add AutoConfigureMockMvc annotation to StudentControllerTest for improved testing

--- src/test/java/pro/elevateme/student_crud/controller/StudentControllerTest.java
+++ src/test/java/pro/elevateme/student_crud/controller/StudentControllerTest.java
@@ -1,9 +1,11 @@
 package pro.elevateme.student_crud.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class StudentControllerTest {
 
     @Test
